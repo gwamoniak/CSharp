2cf9a6f8ce1ee695c4227f70b7d6ab3a75bf56fa
gwamoniak <pictureofme@outlook.com>
2021-02-05 15:41:19 +0100

    Stack initial commit

---
+++ csharp/course/Exercises/Stack/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class Stack : IEnumerable
+    {
+        private object item;
+        private List<object> list = new List<object>();
+
+        public IEnumerator GetEnumerator()
+        {
+            while (list.Any())
+                yield return Pop();
+        }
+
+        internal object Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("List is empty");
+
+            item = list.FirstOrDefault();
+
+            list.RemoveAt(0);
+
+            return item;
+        }
+
+        internal void Push(object obj)
+        {
+            item = obj;
+
+            if (item == null)
+                throw new InvalidOperationException($"{ nameof(item) } is null.There is nothing to Push().");
+
+            list.Insert(0, item);
+        }
+        internal void Clear()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Cannot clear empty list.");
+
+            list.Clear();
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            foreach (var s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
